2cab7dcd77a47623791a8d53479b195f311fd241
Philipp Schmitt <philipp@schmitt.co>
2017-01-24 19:27:54 +0100

    Allow port to be set on runtime

--- liveboxplaytv/liveboxplaytv.py
+++ liveboxplaytv/liveboxplaytv.py
@@ -3,7 +3,6 @@
 
 
 from fuzzywuzzy import process
-import argparse
 import json
 import logging
 import requests
@@ -50,8 +49,9 @@ CHANNEL_EPG_IDS = {'Mosaique': '0'}
 
 
 class LiveboxPlayTv(object):
-    def __init__(self, hostname):
+    def __init__(self, hostname, port=8080):
         self.hostname = hostname
+        self.port = port
         self.CHANNELS = None
 
     @property
@@ -116,7 +116,7 @@ class LiveboxPlayTv(object):
         pass
 
     def rq(self, operation, params=None):
-        url = 'http://{}:8080/remoteControl/cmd'.format(self.hostname)
+        url = 'http://{}:{}/remoteControl/cmd'.format(self.hostname, self.port)
         get_params = {'operation': operation}
         if params:
             get_params.update(params)
@@ -165,10 +165,7 @@ class LiveboxPlayTv(object):
 
     def get_channel_names(self, json_output=False):
         channels = [x['name'] for x in self.get_channels()]
-        if json_output:
-            return json.dumps(channels)
-        for c in channels:
-            print(c)
+        return json.dumps(channels) if json_output else channels
 
     def get_channel_epg_id(self, channel):
         # If the channel start with '#' search by channel number
@@ -202,8 +199,8 @@ class LiveboxPlayTv(object):
         # FIXME We cannot use rq here since requests automatically urlencodes
         # the '*' characters
         # return self.rq('09', {'epg_id': epg_id_str, 'uui': 1})
-        url = 'http://{}:8080/remoteControl/cmd?operation=09&epg_id={}&uui=1'.format(
-            self.hostname, epg_id_str
+        url = 'http://{}:{}/remoteControl/cmd?operation=09&epg_id={}&uui=1'.format(
+            self.hostname, self.port, epg_id_str
         )
         r = requests.get(url)
         r.raise_for_status()
@@ -251,93 +248,7 @@ class LiveboxPlayTv(object):
 
     def event_notify(self):
         # https://www.domotique-fibaro.fr/topic/4444-tv-commande-decodeur-livebox-play-et-gestion-d%C3%A3%C2%A9tat-temps-r%C3%A3%C2%A9el/
-        url = 'http://{}:8080/remoteControl/notifyEvent'.format(self.hostname)
+        url = 'http://{}:{}/remoteControl/notifyEvent'.format(self.hostname, self.port)
         r = requests.get(url)
         r.raise_for_status()
         return r.json()
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='action', help='Action')
-    parser.add_argument(
-        '-H', '--hostname',
-        required=True,
-        help='IP address or hostname of the Livebox Play'
-    )
-    parser.add_argument(
-        '-j', '--json',
-        action='store_true',
-        default=False,
-        required=False,
-        help='Format output as JSON'
-    )
-    parser.add_argument(
-        '-d', '--debug',
-        action='store_true',
-        default=False,
-        required=False,
-        help='Debug mode'
-    )
-    key_parser = subparsers.add_parser('key', help='Press an arbitrary key')
-    key_parser.add_argument('key', help='Name or ID of the key to press')
-    vol_parser = subparsers.add_parser('vol', help='Volume Control')
-    vol_parser.add_argument('volume_action', choices=['up', 'down', 'mute'])
-    subparsers.add_parser('info', help='Get info')
-    subparsers.add_parser('state', help='Get the current state (on or off)')
-    subparsers.add_parser('on', help='Turn the Livebox Play appliance on')
-    subparsers.add_parser('off', help='Turn the Livebox Play appliance off')
-    channel_parser = subparsers.add_parser(
-        'channel', help='Get or set the current channel'
-    )
-    channel_parser.add_argument('CHANNEL', nargs='?')
-    # Debuggign methods
-    subparsers.add_parser('notify', help='Wait and notify of new events')
-    op_parser = subparsers.add_parser('op', help='[DEBUG] Send request')
-    op_parser.add_argument('OPERATION', help='Operation')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
-    output = ''
-    l = LiveboxPlay(args.hostname)
-
-    if args.action == 'info':
-        output = l.info
-    elif args.action == 'state':
-        output = 'on' if l.state() else 'off'
-    elif args.action == 'on':
-        output = l.turn_on()
-    elif args.action == 'off':
-        output = l.turn_off()
-    elif args.action == 'key':
-        output = l.press_key(args.key)
-    elif args.action == 'vol':
-        if args.volume_action == 'up':
-            output = l.volume_up()
-        elif args.volume_action == 'down':
-            output = l.volume_down()
-        elif args.volume_action == 'mute':
-            output = l.mute()
-    elif args.action == 'channel':
-        if args.CHANNEL:
-            if args.CHANNEL.lower() == 'list':
-                output = l.get_channel_names(args.json)
-            else:
-                output = l.set_channel(args.CHANNEL)
-        else:
-            output = l.get_current_channel_name()
-    elif args.action == 'notify':
-        output = l.event_notify()
-    elif args.action == 'op':
-        output = l.rq(args.OPERATION)
-
-    if output:
-        if args.json:
-            from pprint import pprint
-            pprint(output)
-        else:
-            print(output)
