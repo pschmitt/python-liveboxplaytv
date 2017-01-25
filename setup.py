b0d293ee591961119cabc5da89332ed548438376
Philipp Schmitt <philipp@schmitt.co>
2017-01-25 21:11:52 +0100

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='liveboxplaytv',
-    version='1.1.0',
+    version='1.2.0',
     license='GPL3',
     description='Python bindings for the Orange Livebox Play TV appliance',
     long_description=open('README.rst').read(),
