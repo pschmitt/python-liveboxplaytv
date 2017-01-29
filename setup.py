4795baea89dcf19afd9320d8001c8583f7fe4b21
Philipp Schmitt <philipp@schmitt.co>
2017-01-29 10:22:07 +0100

    Version 1.4.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='liveboxplaytv',
-    version='1.4.3',
+    version='1.4.4',
     license='GPL3',
     description='Python bindings for the Orange Livebox Play TV appliance',
     long_description=open('README.rst').read(),
@@ -11,7 +11,8 @@ setup(
     author_email='philipp@schmitt.co',
     url='https://github.com/pschmitt/python-liveboxplaytv',
     packages=find_packages(),
-    install_requires=['requests', 'fuzzywuzzy', 'python-Levenshtein'],
+    install_requires=['requests', 'fuzzywuzzy', 'python-Levenshtein',
+                      'wikipedia'],
     entry_points={
         'console_scripts': ['liveboxplaytv=liveboxplaytv.cli:main']
     }
