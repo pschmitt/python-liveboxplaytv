cb088ebd6b8b0ea3e10208252dd218b81ec072a7
Philipp Schmitt <philipp@schmitt.co>
2017-10-18 20:53:27 +0200

    Require an up to date version of pyteleloisirs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='liveboxplaytv',
-    version='1.5.1',
+    version='1.5.2',
     license='GPL3',
     description='Python bindings for the Orange Livebox Play TV appliance',
     long_description=open('README.rst').read(),
@@ -11,7 +11,7 @@ setup(
     author_email='philipp@schmitt.co',
     url='https://github.com/pschmitt/python-liveboxplaytv',
     packages=find_packages(),
-    install_requires=['fuzzywuzzy', 'python-Levenshtein', 'pyteleloisirs',
+    install_requires=['fuzzywuzzy', 'python-Levenshtein', 'pyteleloisirs>=1.4',
                       'requests', 'wikipedia'],
     entry_points={
         'console_scripts': ['liveboxplaytv=liveboxplaytv.cli:main']
